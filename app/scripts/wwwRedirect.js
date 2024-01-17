0977186d716bba968ace2df089e00adfdf5b4eb2
Colton Weatherston <colton_w@shaw.ca>
2024-01-16 20:46:55 -0800

    Updated wwwRedirect.js

--- app/scripts/wwwRedirect.js
+++ app/scripts/wwwRedirect.js
@@ -1,3 +1,3 @@
 if (location.hostname == "www.bchwyevents.app") {
-  window.location.href("https://bchwyevents.app" + location.pathname);
+  window.location.href = "https://bchwyevents.app" + location.pathname;
 }
